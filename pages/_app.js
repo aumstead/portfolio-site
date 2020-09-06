91f5daf0c6cfc350f057a29e46f9990ecc4c676a
Andrew Umstead <andrewumstead@MacBook-Air.local>
2020-09-07 05:56:51 +0800

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
-// import App from 'next/app'
 import "../styles.css";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import DarkModeState from "../contexts/darkMode/DarkModeState";
 import MobileState from "../contexts/mobile/MobileState";
 import "prismjs/themes/prism-tomorrow.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <DarkModeState>
       <MobileState>
