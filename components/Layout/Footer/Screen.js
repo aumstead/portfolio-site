e7d3cbe25b311ab5060168ad587df33e1156502d
Andrew Umstead <andrewumstead@MacBook-Air.local>
2020-09-08 17:47:31 +0800

    add github icon in footer

--- components/Layout/Footer/Screen.js
+++ components/Layout/Footer/Screen.js
@@ -1,45 +1,76 @@
-import styles from './Screen.module.scss'
-import { useEffect, useRef } from 'react'
+import styles from "./Screen.module.scss";
+import { useEffect, useRef } from "react";
 
 function Screen({ triggerTimeline }) {
   const isInitialRender = useRef(true);
-  const tl = useRef(null)
+  const tl = useRef(null);
 
   useEffect(() => {
-    tl.current = gsap.timeline()
-    tl.current.to("#line1", {duration: 1, text: "Email me!", delay: 1})
-      .to("#line2", {duration: 3, text: "andrew.umstead@gmail.com"})
-      .to("#linkedin", {duration: .8, opacity: 1})
-      .to("#twitter", {duration: .8, opacity: 1})
-      .to("#resume", {duration: 1, text: "resume.pdf"})
-    tl.current.pause()
-  }, [])
+    tl.current = gsap.timeline();
+    tl.current
+      .to("#line1", { duration: 1, text: "Email me!", delay: 1 })
+      .to("#line2", { duration: 3, text: "andrew.umstead@gmail.com" })
+      .to("#linkedin", { duration: 0.8, opacity: 1 })
+      .to("#githubPixel", { duration: 0.8, opacity: 1 })
+      .to("#resume", { duration: 1, text: "resume.pdf" });
+    tl.current.pause();
+  }, []);
 
   useEffect(() => {
     if (isInitialRender.current) {
       isInitialRender.current = false;
-      return
+      return;
     }
 
-    if(triggerTimeline) {
-      tl.current.restart()
+    if (triggerTimeline) {
+      tl.current.restart();
     } else {
-      tl.current.seek(0)
-      tl.current.pause()
+      tl.current.seek(0);
+      tl.current.pause();
     }
-
-  }, [triggerTimeline])
+  }, [triggerTimeline]);
   return (
     <div className={styles.container}>
       <p className={styles.text} id="line1"></p>
-      <a className={`${styles.text} ${styles.underline}`} id="line2" href="mailto:andrew.umstead@gmail.com"></a>
+      <a
+        className={`${styles.text} ${styles.underline}`}
+        id="line2"
+        href="mailto:andrew.umstead@gmail.com"
+      ></a>
       <div className={styles.socialMediaContainer}>
-        <a id="linkedin" className={styles.linkedInAnchor} href="#"><img className={styles.linkedin} src="/images/linkedin.png" alt="LinkedIn icon"/></a>
-        <a id="twitter" className={styles.twitterAnchor} href="#"><img className={styles.twitter} src="/images/twitter.png" alt="Twitter icon"/></a>
+        <a
+          id="linkedin"
+          className={styles.linkedInAnchor}
+          href="https://linkedin.com/in/aumstead"
+          target="_blank"
+        >
+          <img
+            className={styles.linkedin}
+            src="/images/linkedin.png"
+            alt="LinkedIn icon"
+          />
+        </a>
+        <a
+          id="githubPixel"
+          className={styles.githubAnchor}
+          href="https://github.com/aumstead"
+          target="_blank"
+        >
+          <img
+            className={styles.github}
+            src="/images/githubPixel.png"
+            alt="Github icon"
+          />
+        </a>
       </div>
-      <a className={`${styles.text} ${styles.underline}`} id="resume" href="/resume.pdf" target="_blank"></a>
+      <a
+        className={`${styles.text} ${styles.underline}`}
+        id="resume"
+        href="/resume.pdf"
+        target="_blank"
+      ></a>
     </div>
-  )
+  );
 }
 
-export default Screen
+export default Screen;
